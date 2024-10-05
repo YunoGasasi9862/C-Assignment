c230ba96fd3c9a9aebf4bd6e53142cff3605f415
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2024-10-05 16:35:30 +0300

    Assignment :8ball:

---
+++ Yuki/BLL/Models/StudentModel.cs
@@ -0,0 +1,21 @@
+﻿using BLL.DLL;
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        private Student? Student { get; set; }
+
+        public int? Id { get => Student?.Id; }
+
+        public string? Name { get => Student?.Name; }
+
+        public string? Surname { get => Student?.Surname;}
+
+        public DateTime? BirthDate { get => Student?.BirthDate; }
+
+        public decimal? Gpa { get => Student?.Gpa; }
+
+        public string? FullName { get => $"{Student?.Name} {Student?.Surname}"; }
+    }
+}
